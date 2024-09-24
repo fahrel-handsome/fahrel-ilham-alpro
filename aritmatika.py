66385edec04ca6e620c84178e780ef79283306cf
fahrel-handsome <24091397024@mhs.unesa.ac.id>
2024-09-24 23:23:34 +0700

    Add files via upload

---
+++ aritmatika.py
@@ -0,0 +1,22 @@
+#input bilangan
+bil_1 = int (input("masukan nilai untuk bilangan pertama: "))
+bil_2 = int (input("masukan nilai untuk bilangan kedua: "))
+
+#proses penjumlahan
+penjumlahan = bil_1+bil_2
+
+#proses pengurangan
+pengurangan = bil_1-bil_2
+
+#proses perkalian
+perkalian = bil_1*bil_2
+
+#proses pembagian
+pembagian = bil_1/bil_2
+
+#output hasil proses
+print("hasil penjumlahan:", penjumlahan)
+print("hasil pengurangan:", pengurangan)
+print("hasil perkalian:", perkalian)
+print("hasil pembagian:", pembagian)
+
