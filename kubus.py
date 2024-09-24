417bf8200ff11ef456d4b259fc6b70e754bac4bf
fahrel-handsome <24091397024@mhs.unesa.ac.id>
2024-09-24 23:27:26 +0700

    Add files via upload

---
+++ kubus.py
@@ -0,0 +1,9 @@
+import math
+
+sisi = int(input("masukan nilai sisi kubus: "))
+
+luas = 6*math.pow(sisi, 2)
+volume = math.pow(sisi, 3)
+
+print("luas permukaan kubus adalah:", luas)
+print("volume kubus adalah:", volume)
