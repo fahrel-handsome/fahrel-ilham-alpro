4cf2f71059d33a5077f74e2dc40a344848e149bf
fahrel-handsome <24091397024@mhs.unesa.ac.id>
2024-09-24 23:27:59 +0700

    Add files via upload

---
+++ lingkaran.py
@@ -0,0 +1,9 @@
+import math 
+pi = math.pi
+
+r = int(input("masukan jari jari: "))
+luas = pi*r**2
+keliling = 2*pi*r
+
+print("nilai dari luas lingkaran:", (luas))
+print("nilai dari keliling lingkaran:", (keliling))
